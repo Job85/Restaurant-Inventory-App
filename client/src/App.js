f7647d81b9d6266c9b6518ce09bc70bc718300e0
Job85 <julianjernigan@ymail.com>
2022-05-23 12:23:03 -0400

    muted usestate & useeffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+// { useState, useEffect }
+import { Routes, Route, } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 // import Items from './components/Items';
 // import ItemForm from './components/ItemForm';
 // import ItemDetails from './components/ItemDetails';
-import axios from 'axios';
-import { BASE_URL } from './globals';
+// import axios from 'axios';
+// import { BASE_URL } from './globals';
 
 export const Context = React.createContext({ value: null, setValue: () => { } });
 
 const App = () => {
   // hook to populate new items in ItemForm.jsx
-  let [newItem, setNewItem] = useState({
-    location: '',
-    category: '',
-    item: '',
-    size: '',
-    count: ''
-  })
+  // let [newItem, setNewItem] = useState({
+  //   location: '',
+  //   category: '',
+  //   item: '',
+  //   size: '',
+  //   count: ''
+  // })
 
-  const handleChange = (e) => {
-    setNewItem({ ...newItem, [e.target.name]: e.target.value })
-  }
-  // event handler passed as prop to submit new items in ItemForm.jsx
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let postItem = axios.post(`${BASE_URL}/items`, newItem)
-    return postItem
-  }
+  // const handleChange = (e) => {
+  //   setNewItem({ ...newItem, [e.target.name]: e.target.value })
+  // }
+  // // event handler passed as prop to submit new items in ItemForm.jsx
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   let postItem = axios.post(`${BASE_URL}/items`, newItem)
+  //   return postItem
+  // }
 
-  const handleUpdate = (id) => {
-    let editItem = axios.get(`${BASE_URL}/items/${id}`)
-    return editItem
-  }
+  // const handleUpdate = (id) => {
+  //   let editItem = axios.get(`${BASE_URL}/items/${id}`)
+  //   return editItem
+  // }
 
   return (
     <div className="App">
