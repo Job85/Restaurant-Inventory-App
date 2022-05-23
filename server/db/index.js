8bb1f455e8f728a3cd0e41a5beded61237d3d234
Job85 <julianjernigan@ymail.com>
2022-05-23 12:03:40 -0400

    updated dbURL

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose')
-require('dotenv').config() // Add this line
-
-let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/restaurantDatabase'
+require('dotenv').config()
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/inventoryDatabase'
 
 mongoose
     .connect(dbUrl)
     .then(() => {
-        console.log('Successfully connected to MongoDB!')
+        console.log('Successfully connected to MongoDB.')
     })
     .catch((e) => {
         console.error('Connection error', e.message)
