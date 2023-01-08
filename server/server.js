1f1c8d858084ef832f3db74c0470774217b9e7fc
Job85 <julianjernigan@ymail.com>
2023-01-08 14:17:54 -0500

    setup server to use: cors, express, logger, api. setup root route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const cors = require('cors')
+const logger = require('morgan')
+const db = require('./db')
 
 const app = express()
 
 const PORT = process.env.PORT || 3001
 
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+app.use('/api', AppRouter)
+app.get('/', (req, res) => {
+    res.send('This is root!')
+})
+
 app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}`)
 })
-
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
