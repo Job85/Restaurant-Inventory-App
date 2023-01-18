bd0318d3335fbc3876880a964554f05f8d636f07
Job85 <julianjernigan@ymail.com>
2023-01-18 14:02:55 -0500

    corrected put route with callback function 'putItemById'

--- server/routes/ItemRouter.js
+++ server/routes/ItemRouter.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/ItemController')
 Router.get('/', controller.getItems)
 Router.get('/:id', controller.getItemById)
 Router.post('/create', controller.postItem)
-Router.put('/upate', controller)
+Router.put('/upate/:id', controller.putItemById)
 Router.delete('/delete/:id', controller.deleteItemById)
 
 module.exports = Router
