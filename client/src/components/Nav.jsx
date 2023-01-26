90d8932eccc703c60491a7edaf61994e6e2dcc1f
Job85 <julianjernigan@ymail.com>
2023-01-25 22:58:56 -0500

    replaced text in Link with FontAwesome icons

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faTableList, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
 
     return (
         <nav className="navbar">
             <div>
-                <Link to='/'>Home</Link>
-                <Link to='items'>Items</Link>
-                <Link to='/new'>Add Item</Link>
+                <Link to='/'><FontAwesomeIcon icon={faHouse} /></Link>
+                <Link to='items'><FontAwesomeIcon icon={faTableList} /></Link>
+                <Link to='/new'><FontAwesomeIcon icon={faCirclePlus} /></Link>
             </div>
         </nav>
     )
