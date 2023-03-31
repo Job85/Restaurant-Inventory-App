68bbff2b475b842f2fa7d80698db76205134f857
Job85 <joulescodinglab@gmail.com>
2023-03-30 21:05:33 -0400

    removed server directory

--- server/models/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const ItemSchema = require('./item')
-
-const Item = mongoose.model('Item', ItemSchema)
-
-module.exports = {
-    Item
-}
--- server/routes/AppRouter.js
+++
@@ -1,6 +0,0 @@
-const Router = require('express').Router()
-const ItemRouter = require('./ItemRouter')
-
-Router.use('/item', ItemRouter)
-
-module.exports = Router
