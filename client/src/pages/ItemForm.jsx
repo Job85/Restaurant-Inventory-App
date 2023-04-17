39b7047852d289d495dd8a3eaac2ebcf8ddad4fd
Job85 <joulescodinglab@gmail.com>
2023-04-17 14:29:00 -0400

    added class to button so it centers on card

--- client/src/pages/ItemForm.jsx
+++ client/src/pages/ItemForm.jsx
@@ -14,42 +14,48 @@ const ItemForm = (props) => {
                         className="homeCard"
                         title="Item Form"
                     >
-                        <input type='text'
+                        <input
+                            type='text'
                             defaultValue={props.newItem.location}
                             onChange={props.handleChange}
                             name={'location'}
                             placeholder={'location'}
                             className='locationInput'
                         />
-                        <input type='text'
+                        <input
+                            type='text'
                             defaultValue={props.newItem.category}
                             onChange={props.handleChange}
                             name={'category'}
                             placeholder={'category'}
                             className='categoryInput'
                         />
-                        <input type='text'
+                        <input
+                            type='text'
                             defaultValue={props.newItem.item_name}
                             onChange={props.handleChange}
                             name={'item_name'}
                             placeholder={'item'}
                             className='itemInput'
                         />
-                        <input type='text'
+                        <input
+                            type='text'
                             defaultValue={props.newItem.description}
                             onChange={props.handleChange}
                             name={'description'}
                             placeholder={'description'}
                             className='descriptionInput'
                         />
-                        <input type='text'
+                        <input
+                            type='text'
                             defaultValue={props.newItem.unit_measure}
                             onChange={props.handleChange}
                             name={'unit_measure'}
                             placeholder={'unit of measure'}
                             className='uomInput'
                         />
-                        <input type='text'
+                        <input
+                            type='text'
                             defaultValue={props.newItem.case_size}
                             onChange={props.handleChange}
                             name={'case_size'}
@@ -62,21 +68,28 @@ const ItemForm = (props) => {
                             name={'count'}
                             placeholder={'count'}
                             className='label-form' /> */}
-                        <input type='text'
+                        <input
+                            type='text'
                             defaultValue={props.newItem.vendor_name}
                             onChange={props.handleChange}
                             name={'vendor_name'}
                             placeholder={'vendor_name'}
                             className='vendorInput'
                         />
-                        <input type='text'
+                        <input
+                            type='text'
                             defaultValue={props.newItem.vendor_code}
                             onChange={props.handleChange}
                             name={'vendor_code'}
                             placeholder={'vendor_code'}
                             className='vcInput'
                         />
-                        <button type="submit">Add Item</button>
+                        <button
+                            type="submit"
+                            className="addButton"
+                        >
+                            Add Item
+                        </button>
                     </Card>
                 </form>
             </>
