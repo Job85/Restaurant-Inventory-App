f8b12e33705c24a85616c552040044487fb7cd6e
Job85 <joulescodinglab@gmail.com>
2023-04-17 03:21:45 -0400

    created Card component rendered inside ItemForm page, styled with grid-areas, removed excess code from ItemForm

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const item = [
+    <b className="location">Location:</b>,
+    <b className="category">Category:</b>,
+    <b className="item">Item:</b>,
+    <b className="description">Description:</b>,
+    <b className="uom">Unit of Measure:</b>,
+    <b className="case">Case Size:</b>,
+    <b className="vendor">Vendor Name:</b>,
+    <b className="vc">Vender Code:</b>
+];
+
+const inventoryItem = item.map((content, index) => <li key={index}>{content}</li>);
+
+const Card = ({ title, children }) => {
+
+    return (
+        <div className="card">
+            <h2>{title}</h2>
+            <div>
+                <ul className="cardUl">{inventoryItem}{children}</ul>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- client/src/pages/ItemForm.jsx
+++ client/src/pages/ItemForm.jsx
@@ -1,101 +1,85 @@
 import React from "react"
+import Card from "../components/Card"
 
 const ItemForm = (props) => {
 
     return (
         <div className='create'>
-            <h1>Create Page</h1>
             <h2>
                 Lets create an item!
             </h2>
-            <div>
+            <>
                 <form onSubmit={props.handleSubmit}>
-                    <ul className='create-card'>
-                        <ul>
-                            <label id='location'>Location:</label>
-                            <input type='text'
-                                defaultValue={props.newItem.location}
-                                onChange={props.handleChange}
-                                name={'location'}
-                                placeholder={'location'}
-                                className='label-form' />
-                        </ul>
-                        <ul>
-                            <label id='category'>Category:</label>
-                            <input type='text'
-                                defaultValue={props.newItem.category}
-                                onChange={props.handleChange}
-                                name={'category'}
-                                placeholder={'category'}
-                                className='label-form' />
-                        </ul>
-                        <ul>
-                            <label id='item_name'>Item:</label>
-                            <input type='text'
-                                defaultValue={props.newItem.item_name}
-                                onChange={props.handleChange}
-                                name={'item_name'}
-                                placeholder={'item'}
-                                className='label-form' />
-                        </ul>
-                        <ul>
-                            <label id='description'>Description:</label>
-                            <input type='text'
-                                defaultValue={props.newItem.description}
-                                onChange={props.handleChange}
-                                name={'description'}
-                                placeholder={'description'}
-                                className='label-form' />
-                        </ul>
-                        <ul>
-                            <label id='unit_measure'>Unit of Measure:</label>
-                            <input type='text'
-                                defaultValue={props.newItem.unit_measure}
-                                onChange={props.handleChange}
-                                name={'unit_measure'}
-                                placeholder={'unit of measure'}
-                                className='label-form' />
-                        </ul>
-                        <ul>
-                            <label id='case_size'>Case Size:</label>
-                            <input type='text'
-                                defaultValue={props.newItem.case_size}
-                                onChange={props.handleChange}
-                                name={'case_size'}
-                                placeholder={'case size'}
-                                className='label-form' />
-                        </ul>
-                        <ul>
-                            <label id='count'>Count:</label>
-                            <input type='number'
-                                defaultValue={props.newItem.count}
-                                onChange={props.handleChange}
-                                name={'count'}
-                                placeholder={'count'}
-                                className='label-form' />
-                        </ul>
-                        <ul>
-                            <label id='vendor_name'>Vendor Name:</label>
-                            <input type='text'
-                                defaultValue={props.newItem.vendor_name}
-                                onChange={props.handleChange}
-                                name={'vendor_name'}
-                                placeholder={'vendor_name'}
-                                className='label-form' />
-                        </ul>
-                        <ul>
-                            <label id='vendor_code'>Vendor Code:</label>
-                            <input type='text'
-                                defaultValue={props.newItem.vendor_code}
-                                onChange={props.handleChange}
-                                name={'vendor_code'}
-                                placeholder={'vendor_code'}
-                                className='label-form' />
-                        </ul>
-                    </ul>
-                    <button type="submit">Add Item</button>
+                    <Card
+                        className="homeCard"
+                        title="Item Form"
+                    >
+                        <input type='text'
+                            defaultValue={props.newItem.location}
+                            onChange={props.handleChange}
+                            name={'location'}
+                            placeholder={'location'}
+                            className='locationInput'
+                        />
+                        <input type='text'
+                            defaultValue={props.newItem.category}
+                            onChange={props.handleChange}
+                            name={'category'}
+                            placeholder={'category'}
+                            className='categoryInput'
+                        />
+                        <input type='text'
+                            defaultValue={props.newItem.item_name}
+                            onChange={props.handleChange}
+                            name={'item_name'}
+                            placeholder={'item'}
+                            className='itemInput'
+                        />
+                        <input type='text'
+                            defaultValue={props.newItem.description}
+                            onChange={props.handleChange}
+                            name={'description'}
+                            placeholder={'description'}
+                            className='descriptionInput'
+                        />
+                        <input type='text'
+                            defaultValue={props.newItem.unit_measure}
+                            onChange={props.handleChange}
+                            name={'unit_measure'}
+                            placeholder={'unit of measure'}
+                            className='uomInput'
+                        />
+                        <input type='text'
+                            defaultValue={props.newItem.case_size}
+                            onChange={props.handleChange}
+                            name={'case_size'}
+                            placeholder={'case size'}
+                            className='caseInput'
+                        />
+                        {/* <input type='number'
+                            defaultValue={props.newItem.count}
+                            onChange={props.handleChange}
+                            name={'count'}
+                            placeholder={'count'}
+                            className='label-form' /> */}
+                        <input type='text'
+                            defaultValue={props.newItem.vendor_name}
+                            onChange={props.handleChange}
+                            name={'vendor_name'}
+                            placeholder={'vendor_name'}
+                            className='vendorInput'
+                        />
+                        <input type='text'
+                            defaultValue={props.newItem.vendor_code}
+                            onChange={props.handleChange}
+                            name={'vendor_code'}
+                            placeholder={'vendor_code'}
+                            className='vcInput'
+                        />
+                        <button type="submit">Add Item</button>
+                    </Card>
                 </form>
-            </div>
+            </>
         </div >
     )
 }
