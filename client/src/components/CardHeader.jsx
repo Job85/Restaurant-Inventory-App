a20c570a62aee707cc3ab991c09ea0d9d90c91e5
Job85 <joulescodinglab@gmail.com>
2023-04-19 12:25:34 -0400

    resest to last commit

---
+++ client/src/components/CardHeader.jsx
@@ -0,0 +1,30 @@
+// import React from "react";
+
+// const CardHeader = (props) => {
+
+//     let banana = document.getElementById("cardHeader").innerHTML.trim();
+//     switch (banana) {
+
+//         case "CreateNewItem":
+//             console.log(document.getElementById("cardHeader").innerHTML);
+//             document.getElementById.innerHTML = "Create A New Item";
+//             console.log(document.getElementById("cardHeader").innerHTML + "(HAS CHANGED)")
+//             break;
+//         case "EditItem":
+//             console.log(document.getElementById("cardHeader").innerHTML);
+//             document.getElementById("cardHeader").innerHTML = `Edit ${props.newItem.item_name}`;
+//             console.log(document.getElementById("cardHeader").innerHTML + "(HAS CHANGED")
+//             break;
+//         default:
+//             console.log(document.getElementById("cardHeader").innerHTML);
+//             document.getElementById.innerHTML = `Default Case`;
+//             console.log(document.getElementById("cardHeader").innerHTML + "(HAS CHANGED")
+
+//     }
+
+//     return (
+//         <h2 id="cardHeader">{banana}</h2>
+//     )
+// }
+
+// export default CardHeader
--- client/src/pages/ItemDetails.jsx
+++ client/src/pages/ItemDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios'
 import { BASE_URL } from '../globals'
-import ItemForm from '../components/ItemForm';
+import ItemForm from './ItemForm';
 
 const ItemDetails = (props) => {
     let navigate = useNavigate()
