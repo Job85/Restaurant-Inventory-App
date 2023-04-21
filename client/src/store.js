56afcde04a0277d3494068e6bf7ec84453dfea9c
Job85 <joulescodinglab@gmail.com>
2023-04-20 23:55:12 -0400

    imported buttonNameReducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cardTitleReducer from './slices/cardTitleSlice';
+import buttonNameReducer from './slices/buttonNameSlice';
 
 export default configureStore({
     reducer: {
         cardTitle: cardTitleReducer,
+        buttonName: buttonNameReducer,
     },
 });
