b805a64a1c528664a6b68c723bf20fe873c69b16
Job85 <joulescodinglab@gmail.com>
2023-04-22 23:31:59 -0400

    created custom hook to set state of item, handleChange, handleSubmit

---
+++ client/src/components/card/ItemHook.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../../globals";
+import axios from "axios";
+
+export const useItemCard = () => {
+    let navigate = useNavigate();
+
+    const [item, setItem] = useState({
+        location: '',
+        category: '',
+        item_name: '',
+        description: '',
+        unit_measure: '',
+        case_size: '',
+        count: '',
+        vendor_name: '',
+        vendor_code: ''
+    })
+
+    const handleChange = (e) => {
+        setItem({ ...item, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(`${BASE_URL}/item/create`, item);
+            navigate("/items");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return {
+        item,
+        handleChange,
+        handleSubmit
+    };
+};
+
+export default useItemCard;
+
+// import { useItemCard } into ItemForm
+// remove props from onSubmit, defaultValue, handleChange
+// change newItem => item
+// call hook above return const { item, handleChange, handleSubmit } = useItemCard()
